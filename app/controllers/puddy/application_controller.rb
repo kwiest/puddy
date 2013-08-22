4748f847e9e580a241b7250ae8c586d89266f8f9
Kyle Wiest <kyle.wiest@gmail.com>
2013-08-21 18:45:22 -0700

    Protect with http basic

--- app/controllers/puddy/application_controller.rb
+++ app/controllers/puddy/application_controller.rb
@@ -1,11 +1,23 @@
 class Puddy::ApplicationController < ActionController::Base
   layout 'puddy/dashboard'
+  before_filter :http_basic_auth
 
   helper_method :accounts
+
 
   protected
 
   def accounts
     Account.order :name
   end
+
+
+  private
+
+  def http_basic_auth
+    authenticate_or_request_with_http_basic do |login, password|
+      login == 'kwiest' && password == 'goducks888'
+    end
+  end
+
 end
