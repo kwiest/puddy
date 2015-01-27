c4276514ba555f89146f3f799837ab3037079a69
Kyle Wiest <kyle.wiest@gmail.com>
2015-01-27 23:42:17 +0000

    Add transaction list views

---
+++ app/controllers/puddy/transactions_controller.rb
@@ -0,0 +1,21 @@
+class Puddy::TransactionsController < Puddy::ApplicationController
+  def index
+    @transaction_state = 'All'
+    @transactions = Transaction.order(:created_at).
+      paginate(page: params[:page], per_page: 25)
+  end
+
+  def successful
+    @transaction_state = 'Successful'
+    @transactions = Transaction.where(success: true).order(:created_at).
+      paginate(page: params[:page], per_page: 25)
+    render 'index'
+  end
+
+  def failed
+    @transaction_state = 'Failed'
+    @transactions = Transaction.where(success: false).order(:created_at).
+      paginate(page: params[:page], per_page: 25)
+    render 'index'
+  end
+end
