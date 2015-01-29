c70c0d81c04777a93ab42ea5e07ff5e18a82ae90
Kyle Wiest <kyle.wiest@gmail.com>
2015-01-29 19:20:34 +0000

    Add camper stats

--- app/models/puddy/dashboard.rb
+++ app/models/puddy/dashboard.rb
@@ -1,10 +1,10 @@
 module Puddy
   class Dashboard
-    def successful_transactions_total
+    def transactions_money_total
       Money.new(transactions_query["sum"]).format
     end
 
-    def successful_transactions_count
+    def transactions_count
       transactions_query["count"]
     end
 
@@ -13,11 +13,33 @@ module Puddy
       @transaction_query ||= Transaction.find_by_sql(sql).first
     end
 
-    def successful_transactions_this_week
+    def transactions_this_week
       Transaction.where(success: true).
-        where(created_at: 1.week.ago..Date.today).
         order(:created_at).
+        where(created_at: 1.week.ago..Date.today).
         count
+    end
+
+
+    def campers_count
+      Camper.confirmed.count
+    end
+
+    def campers_this_week
+      Camper.confirmed.
+        order(:created_at).
+        where(created_at: 1.week.ago..Date.today).
+        count
+    end
+
+    def campers_week_over_week
+      two_weeks_ago = Camper.confirmed.
+        order(:created_at).
+        where(created_at: 1.week.ago..2.weeks.ago).
+        count
+
+      week_over_week = two_weeks_ago - campers_this_week
+      week_over_week >= 0 ? "+#{week_over_week}" : "-#{week_over_week}"
     end
   end
 end
