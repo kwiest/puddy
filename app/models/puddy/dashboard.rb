4c449b513a157f6775d419c45e79fdc240cc1869
Kyle Wiest <kyle.wiest@gmail.com>
2015-01-29 19:03:56 +0000

    Update stats layout

--- app/models/puddy/dashboard.rb
+++ app/models/puddy/dashboard.rb
@@ -12,5 +12,12 @@ module Puddy
       sql = %Q[SELECT SUM("amount_cents"), COUNT(*) FROM "transactions" WHERE "transactions"."success" = 't']
       @transaction_query ||= Transaction.find_by_sql(sql).first
     end
+
+    def successful_transactions_this_week
+      Transaction.where(success: true).
+        where(created_at: 1.week.ago..Date.today).
+        order(:created_at).
+        count
+    end
   end
 end
