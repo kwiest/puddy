637cb32ac92373b8c8e3d2876552ad759abd06a8
Kyle Wiest <kyle.wiest@gmail.com>
2016-03-28 22:05:11 +0000

    Order newest payments first

--- app/controllers/puddy/payments_controller.rb
+++ app/controllers/puddy/payments_controller.rb
@@ -2,7 +2,7 @@ class Puddy::PaymentsController < Puddy::ApplicationController
   def index
     @payment_state = 'All'
     @payments = Payment.where(payment_type: 'Online Credit Card').
-      order(:created_at).
+      order('created_at DESC').
       paginate(page: params[:page], per_page: 25)
   end
 
@@ -14,7 +14,7 @@ class Puddy::PaymentsController < Puddy::ApplicationController
   def successful
     @payment_state = 'Successful'
     @payments = Payment.paid.where(payment_type: 'Online Credit Card').
-      order(:created_at).
+      order('created_at DESC').
       paginate(page: params[:page], per_page: 25)
     render 'index'
   end
@@ -22,7 +22,7 @@ class Puddy::PaymentsController < Puddy::ApplicationController
   def failed
     @payment_state = 'Failed'
     @payments = Payment.declined.where(payment_type: 'Online Credit Card').
-      order(:created_at).
+      order('created_at DESC').
       paginate(page: params[:page], per_page: 25)
     render 'index'
   end
