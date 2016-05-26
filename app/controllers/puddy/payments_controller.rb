f4b48c626bb51ed9ccf7a8c69f900e81892dd239
Kyle Wiest <kyle.wiest@gmail.com>
2016-05-26 23:03:26 +0000

    Remove extra order statement

--- app/controllers/puddy/payments_controller.rb
+++ app/controllers/puddy/payments_controller.rb
@@ -2,7 +2,6 @@ class Puddy::PaymentsController < Puddy::ApplicationController
   def index
     @payment_state = 'All'
     @payments = Payment.where(payment_type: 'Online Credit Card').
-      order('created_at DESC').
       paginate(page: params[:page], per_page: 25)
   end
 
@@ -14,7 +13,6 @@ class Puddy::PaymentsController < Puddy::ApplicationController
   def successful
     @payment_state = 'Successful'
     @payments = Payment.paid.where(payment_type: 'Online Credit Card').
-      order('created_at DESC').
       paginate(page: params[:page], per_page: 25)
     render 'index'
   end
@@ -22,7 +20,6 @@ class Puddy::PaymentsController < Puddy::ApplicationController
   def failed
     @payment_state = 'Failed'
     @payments = Payment.declined.where(payment_type: 'Online Credit Card').
-      order('created_at DESC').
       paginate(page: params[:page], per_page: 25)
     render 'index'
   end
